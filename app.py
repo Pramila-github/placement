ead4aa1ebce70f3ad0a06b0c4628017c6ac940f3
Pramila-github <78983104+Pramila-github@users.noreply.github.com>
2021-04-26 11:27:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 import pickle
 import streamlit as st
-pickle_in = open('model.pkl', 'rb')
-models = pickle.load(pickle_in)
 
+dataframe = pd.read_csv('Placementsample1.csv')
+def convert_to_int(word):
+    word_dict = {'Good':1, 'Bad':0,'yes':1,'no':0,'completed':1,'none':0}
+    return word_dict[word]
+X=dataframe.loc[:,['comSkill','ssc_p','hsc_p','degree_p','No_certi','internships','etest_p','sports','placetest_p']]
+y=dataframe.loc[:,['status']]
+from sklearn.ensemble import RandomForestClassifier
+regressor = RandomForestClassifier(n_estimators=10,criterion='entropy')
+#Fitting model with training data
+regressor=regressor.fit(X, y)
 
 
 def home():
