bea5a6c9e397dfc98b91bde6c3663722cd571af9
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-07 04:00:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -104,7 +104,7 @@ def main():
     degree_t1 =convert_to_int(degree_t)
     degree_p = st.slider('Degree percentage', 0, 1, 100)
     #No_certi = st.text_input('Number of certifications 🏆📜')
-    No_certi = st.number_input('Number of certifications 🏆📜', min_value=0, max_value=100, value=5, step=1)
+    No_certi = st.number_input('Number of certifications 🏆📜', min_value=0, max_value=100, value=None, step=1)
     internships = st.radio("Internships 👨‍💻", ('completed', 'none'))
     internships1 = convert_to_int(internships)
     etest_p = st.slider('E-tests(aptitude)  🏁', 0, 1, 100)
