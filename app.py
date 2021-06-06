018bd2b8ff82739a7c030742bfac7f0cc9d6d842
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-07 03:47:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,7 +58,7 @@ pickle.dump(regressor, open('final_model.pkl','wb'))
 # Loading model to compare the results
 #print(model.predict([[0,convert_to_int('Good'),39,0,58,0,0,58,0,4,convert_to_int('none'),58,58,convert_to_int('No')]]))
 st.beta_set_page_config(initial_sidebar_state="expanded")
-st.markdown(f'''<style>.sidebar .sidebar-content {{width: 600px;}}</style>''',unsafe_allow_html=True)
+st.markdown(f'''<style>.sidebar .sidebar-content {{width: 400px;}}</style>''',unsafe_allow_html=True)
 models = pickle.load(open('final_model.pkl', 'rb'))
 
 def home():
