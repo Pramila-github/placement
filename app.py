f576a560404d988d86972f22438ceb2e70bdbf56
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-07 03:09:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def main():
                     paragraph.text=paragraph.text.replace("lang",lang)
                 if 'ext' in paragraph.text: 
                     paragraph.text=paragraph.text.replace("ext",ex)
-                
+            open('Resume.docx', "w")    
             doc.save('Resume.docx') 
             st.markdown(get_binary_file_downloader_html('Resume.docx', 'Your Resume'), unsafe_allow_html=True)  
 
