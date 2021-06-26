758924fca8e7139fdd6ca66bb38468790e7570ab
deep-wind <78983104+deep-wind@users.noreply.github.com>
2021-06-26 20:36:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -54,7 +54,7 @@ rand_forest=rand_forest.fit(X_train,Y_train)
 print(rand_forest.predict([[0,1,100.00,0,100.33,1,0,100.4,1,6,0,86,66.28,0]]))
 
 # Saving model to disk
-pickle.dump(regressor, open('final_model.pkl','wb'))
+pickle.dump(rand_forest, open('final_model.pkl','wb'))
 # Loading model to compare the results
 #print(model.predict([[0,convert_to_int('Good'),39,0,58,0,0,58,0,4,convert_to_int('none'),58,58,convert_to_int('No')]]))
 st.beta_set_page_config(initial_sidebar_state="expanded")
