a2fdcb4a4ad5e9e669e9063a10767c2c2e58dd1e
deep-wind <78983104+deep-wind@users.noreply.github.com>
2022-09-18 21:29:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ print(rand_forest.predict([[0,1,100.00,0,100.33,1,0,100.4,1,6,0,86,66.28,0]]))
 pickle.dump(rand_forest, open('final_model.pkl','wb'))
 # Loading model to compare the results
 #print(model.predict([[0,convert_to_int('Good'),39,0,58,0,0,58,0,4,convert_to_int('none'),58,58,convert_to_int('No')]]))
-st.beta_set_page_config(initial_sidebar_state="expanded")
+st.set_page_config(initial_sidebar_state="expanded")
 #st.markdown(f'''<style>.sidebar .sidebar-content {{width: 375px;}}</style>''',unsafe_allow_html=True)
 models = pickle.load(open('final_model.pkl', 'rb'))
 
